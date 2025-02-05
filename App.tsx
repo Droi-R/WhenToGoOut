edafdf9900f5c2d64fc2e1434e3ce4874c69d3a0
droi <silenceg86@gmail.com>
2025-02-05 13:59:36 +0900

    컴포즈 전체화면 불러오기까지

--- App.tsx
+++ App.tsx
@@ -6,112 +6,104 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+// import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
+  // ScrollView,
+  // StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  // Text,
+  // useColorScheme,
   View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  // Colors,
+  // DebugInstructions,
+  // Header,
+  // LearnMoreLinks,
+  // ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 // import {requireNativeComponent} from 'react-native';
 
-import { requireNativeComponent, ViewStyle } from 'react-native';
+import {requireNativeComponent, ViewStyle} from 'react-native';
 
 type MyComposeViewProps = {
-    style?: ViewStyle;
+  style?: ViewStyle;
 };
 
-const MyComposeView = requireNativeComponent<MyComposeViewProps>('MyComposeView');
+const MyComposeView =
+  requireNativeComponent<MyComposeViewProps>('MyComposeView');
 
 // const MyComposeView = requireNativeComponent('MyComposeView');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+// function Section({children, title}: SectionProps): React.JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View style={styles.container}>
-            <MyComposeView
-                style={{
-                    width: 300,
-                    height: 200,
-                }}
-            />
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.fullSize}>
+      {/*<StatusBar*/}
+      {/*  barStyle={isDarkMode ? 'light-content' : 'dark-content'}*/}
+      {/*  backgroundColor={backgroundStyle.backgroundColor}*/}
+      {/*/>*/}
+      <View style={styles.fullSize}>
+        <MyComposeView style={styles.fullSize} />
+      </View>
+      {/*<ScrollView*/}
+      {/*  contentInsetAdjustmentBehavior="automatic"*/}
+      {/*  style={backgroundStyle}>*/}
+      {/*  <Header />*/}
+      {/*<View*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: isDarkMode ? Colors.black : Colors.white,*/}
+      {/*  }}>*/}
+      {/*  <Section title="Step One">*/}
+      {/*    Edit <Text style={styles.highlight}>App.tsx</Text> to change this*/}
+      {/*    screen and then come back to see your edits.*/}
+      {/*  </Section>*/}
+      {/*  <Section title="See Your Changes">*/}
+      {/*    <ReloadInstructions />*/}
+      {/*  </Section>*/}
+      {/*  <Section title="Debug">*/}
+      {/*    <DebugInstructions />*/}
+      {/*  </Section>*/}
+      {/*  <Section title="Learn More">*/}
+      {/*    Read the docs to discover what to do next:*/}
+      {/*  </Section>*/}
+      {/*  <LearnMoreLinks />*/}
+      {/*</View>*/}
+      {/*</ScrollView>*/}
     </SafeAreaView>
   );
 }
@@ -121,6 +113,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  fullSize: {
+    flex: 1,
   },
   sectionContainer: {
     marginTop: 32,
